9287b60d728480a3dc3e4314723fc536769e407a
haju35 <hajirasiraj217@gmail.com>
2024-11-08 11:12:07 -0800

    axios updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //deploy on render
-  baseURL: "https://amazon-api-deploy-01j2.onrender.com/"});
+  baseURL: "https://amazon-api-deploy-2-k8zl.onrender.com/"});
 
 export { axiosInstance };
